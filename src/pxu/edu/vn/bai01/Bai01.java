9e2c76f8d93f0904532bac3a90bbd5a772b5e92a
Trung Nguyên <Trung Nguyn@LAPTOP-KCPRU6H4>
2023-06-30 14:16:58 +0700

    thay doi lan 2

---
+++ src/pxu/edu/vn/bai01/Bai01.java
@@ -0,0 +1,16 @@
+package pxu.edu.vn.bai01;
+
+public class Bai01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Xác định số bước lặp
+		int n = 5;
+		// Vòng lặp while
+		while(n>0) {
+			System.out.println("Hello");
+			n=n-1;
+		}// Kết thúc vòng lặp while thứ 1XjXj
+	}
+
+}
